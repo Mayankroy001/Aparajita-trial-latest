437e85680bfeb1cf0d7867fa7af38b4accf7d290
Mayankroy001 <157673205+Mayankroy001@users.noreply.github.com>
2026-02-11 22:14:06 +0530

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -30,7 +30,7 @@ async function callWithRetry<T>(fn: () => Promise<T>, maxRetries: number = 3): P
  * Verifies that a clear human face is present in the image for account security.
  * Now supports both male and female users.
  */
-const mayank_api="AIzaSyBmD4RA2xQMb02Zno-_EBchgT9fMhy5Kaw"
+const mayank_api="AIzaSyCA_pzqNDCg2R-8uBcSNLorCU82Etngnp4"
 export const verifyIdentity = async (base64Image: string): Promise<boolean> => {
 const ai = new GoogleGenAI({ apiKey: mayank_api });  
   return callWithRetry(async () => {
